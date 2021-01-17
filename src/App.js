38fde5c5479703d4e1761a08376e99b2efa9e6c5
crossfoot <gross.samuelf@gmail.com>
2021-01-17 09:27:45 -0700

    added a 'hooks' file for grabbing calpoly subreddit data, and began working on connecting the front and back end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Table from './Table.js'
-import Form from './Form';
+import Form from './Form.js';
+import FetchDemo from './FetchDemo.js'
+import axios from 'axios'
 
 class App extends Component {
    state = { 
@@ -17,8 +19,37 @@ class App extends Component {
     })
   }
 
+  componentDidMount() {
+    axios.get('http://localhost:5000/users')
+      .then(res => {
+        const characters = res.data.users_list;
+        this.setState({ characters });
+      })
+      .catch(function (error) {
+        //Not handling, just logging
+        console.log(error);
+      });
+  }
+
+  makePostCall(character) {
+    return axios.post('http://localhost:5000/users', character)
+      .then(function (response) {
+        console.log(response);
+        this.setstate(response.data);
+        return (response.status == 201);
+      })
+      .catch(function (error) {
+        console.log(error);
+        return false;
+      });
+  }
+
   handleSubmit = character => {
-    this.setState({characters: [...this.state.characters, character] })
+    this.makePostCall(character).then( callResult => {
+      if (callResult == true) {
+        this.setState({characters: [...this.state.characters, character] });
+      }
+    });
   }
 
   render() {
@@ -28,6 +59,7 @@ class App extends Component {
       <div className="Container">
         <Table characterData={characters} removeCharacter={this.removeCharacter}/>
         <Form handleSubmit={this.handleSubmit} />
+        <FetchDemo subreddit={"calpoly"}/> 
       </div>
     )
   }
